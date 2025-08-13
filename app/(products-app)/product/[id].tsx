9627fb5ca6bfca3f4a19c6945263240661232fb3
josejulianfigueroaguerra <josejulianfigueroaguerra@gmail.com>
2025-08-13 10:16:13 -0400

    save images from camera, gallery

--- app/(products-app)/product/[id].tsx
+++ app/(products-app)/product/[id].tsx
@@ -1,33 +1,51 @@
 import { useEffect } from 'react';
 import {
   View,
-  Text,
   KeyboardAvoidingView,
   ScrollView,
   Platform,
   ActivityIndicator,
 } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import { Redirect, useLocalSearchParams, useNavigation } from 'expo-router';
+import {
+  Redirect,
+  router,
+  useLocalSearchParams,
+  useNavigation,
+} from 'expo-router';
 import { ThemedView } from '@/presentation/theme/components/ThemedView';
 import ThemedTextInput from '@/presentation/theme/components/ThemedTextInput';
-import ThemedButtonGroup from '@/presentation/theme/components/ThemedButtonGroup';
-
+import { useProduct } from '@/presentation/products/hooks/useProduct';
+import ProductImages from '@/presentation/products/components/ProductImages';
+import ThemeButtonGroup from '@/presentation/theme/components/ThemedButtonGroup';
 import ThemedButton from '@/presentation/theme/components/ThemedButton';
 import { Formik } from 'formik';
 import { Size } from '@/core/products/interfaces/product.interface';
-import { useProduct } from '@/presentation/products/hooks/useProduct';
-import ProductImages from '@/presentation/products/components/ProductImages';
+import MenuIconButton from '@/presentation/theme/components/MenuIconButton';
+import { useCameraStore } from '@/presentation/store/useCameraStore';
+import { RefreshControl } from 'react-native-gesture-handler';
 
 const ProductScreen = () => {
+  const { selectedImages, clearImages } = useCameraStore();
+
   const { id } = useLocalSearchParams();
   const navigation = useNavigation();
 
   const { productQuery, productMutation } = useProduct(`${id}`);
 
   useEffect(() => {
+    return () => {
+      clearImages();
+    };
+  }, []);
+
+  useEffect(() => {
     navigation.setOptions({
-      headerRight: () => <Ionicons name="camera-outline" size={25} />,
+      headerRight: () => (
+        <MenuIconButton
+          onPress={() => router.push('/camera')}
+          icon="camera-outline"
+        />
+      ),
     });
   }, []);
 
@@ -54,13 +72,26 @@ const ProductScreen = () => {
   const product = productQuery.data!;
 
   return (
-    <Formik initialValues={product} onSubmit={productMutation.mutate}>
+    <Formik initialValues={product} onSubmit={ (productLike) => 
+    productMutation.mutate({
+      ...productLike,
+      images: [... productLike.images, ...selectedImages]
+    })}>
       {({ values, handleSubmit, handleChange, setFieldValue }) => (
         <KeyboardAvoidingView
           behavior={Platform.OS === 'ios' ? 'padding' : undefined}
         >
-          <ScrollView>
-            <ProductImages images={values.images} />
+          <ScrollView
+          refreshControl = {
+            <RefreshControl
+              refreshing={ productQuery.isFetching }
+              onRefresh= { async() => {
+                await productQuery.refetch();
+              } }
+            ></RefreshControl>
+          }
+          >
+            <ProductImages images={[...product.images, ...selectedImages]} />
 
             <ThemedView style={{ marginHorizontal: 10, marginTop: 20 }}>
               <ThemedTextInput
@@ -114,7 +145,7 @@ const ProductScreen = () => {
                 marginHorizontal: 10,
               }}
             >
-              <ThemedButtonGroup
+              <ThemeButtonGroup
                 options={['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL']}
                 selectedOptions={values.sizes}
                 onSelect={(selectedSize) => {
@@ -128,7 +159,7 @@ const ProductScreen = () => {
                 }}
               />
 
-              <ThemedButtonGroup
+              <ThemeButtonGroup
                 options={['kid', 'men', 'women', 'unisex']}
                 selectedOptions={[values.gender]}
                 onSelect={(selectedOption) =>
--- core/products/actions/create-update-product.action.ts
+++ core/products/actions/create-update-product.action.ts
@@ -14,14 +14,57 @@ export const updateCreateProduct = (product: Partial<Product>) => {
   return createProduct(product);
 };
 
+const prepareImages = async( images: string[] ): Promise<string[]> => {
+
+  const fileImages = images.filter ( (image) => image.startsWith('file'));
+  const currentImages = images.filter ( (image) => !image.startsWith('file'));
+
+    console.log('fileImages', fileImages);
+      console.log('currentImages', currentImages);
+  if(fileImages.length > 0) {
+    const uploadPromises = fileImages.map( uploadImage );
+    const uploadedImages = await Promise.all( uploadPromises );
+  console.log('uploadedImages', uploadedImages);
+    currentImages.push( ... uploadedImages);
+  }
+  console.log('currentImagesDEF', currentImages);
+  return currentImages.map( img => img.split('/').pop()!);
+}
+
+const uploadImage = async( image: string ): Promise<string> => {
+
+  const formData = new FormData() as any;
+
+  formData.append('file', {
+    uri: image,
+    type: 'image/jpeg',
+    name: image.split('/').pop()
+  }
+  )
+  const { data } = await productsApi.post<{secureUrl: string}>(
+    '/files/product',
+    formData,
+    {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    }
+  )
+  return data.secureUrl;
+}
+
 const updateProduct = async (product: Partial<Product>) => {
   const { id, images = [], productCategory,  user, ...rest } = product;
 
   try {
+      console.log('images', images);
+    const checkedImages = await prepareImages(images);
+    console.log('checkedImages', checkedImages);
+     console.log('rest', JSON.stringify(rest));
     const { data } = await productsApi.patch<Product>(`/products/${id}`, {
-      // todo: images
       ...rest,
-      idProductCategory: productCategory.id
+      idProductCategory: productCategory.id,
+      images: checkedImages
     });
 
     return data;
@@ -34,11 +77,12 @@ async function createProduct(product: Partial<Product>) {
   const { id, images = [], productCategory, user, ...rest } = product;
 
   try {
-  
+    const checkedImages = await prepareImages(images);
+
     const { data } = await productsApi.post<Product>(`/products`, {
-      // todo: images
       ...rest,
-      idProductCategory: "277824d4-5039-4190-8a54-9f17d242245a"
+      idProductCategory: "277824d4-5039-4190-8a54-9f17d242245a",
+      images: checkedImages
     });
 
     return data;
